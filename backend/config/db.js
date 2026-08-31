256fbe581cdec6e50bca10dcec5f51d3cd6e07d6
Bhumika <bhumikajainjain087@gmail.com>
2026-08-31 15:07:34 +0530

    connect to mongodb at cloud

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
 
-    console.log("MongoDB connected successfully");
-  } catch (err) {
-    console.log("MongoDB connection failed:", err.message);
-  }
+        console.log("MongoDB connected successfully");
+    } catch (err) {
+        console.log("MongoDB connection failed:", err.message);
+    }
 };
 
 module.exports = connectDb;
